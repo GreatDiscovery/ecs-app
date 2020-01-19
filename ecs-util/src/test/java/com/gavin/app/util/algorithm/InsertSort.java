e1e4c070a36ea1ca2a8f55b7010d190505ceb17f
gavin <1303535630@qq.com>
2020-01-19 17:43:15 +0800

    add: 插入排序
    TODO：
    双轴快排
    准备数组读取部分和发布

---
+++ ecs-util/src/test/java/com/gavin/app/util/algorithm/InsertSort.java
@@ -0,0 +1,33 @@
+package com.gavin.app.util.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * Arrays.sort()里的插入排序，在数组长度小于47时使用
+ *
+ * @author: Gavin
+ * @date: 2020/1/19 16:59
+ * @description:
+ */
+public class InsertSort {
+
+    public static void insertSort(int[] a, int left, int right) {
+        // 一次循环中定下1个位置，这里必须用++i
+        for (int i = left, j = i; i < right; j = ++i) {
+           int ai = a[i + 1];
+           while (ai < a[j]) {
+               a[j + 1] = a[j];
+               if (j-- == left) {
+                   break;
+               }
+           }
+           a[j + 1] = ai;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8, 2, 5, 3, 1};
+        insertSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+}

76626d65eb1ffaf161be058f69e3ba93f6af83bb
宋博文 <songbowen6@jd.com>
2021-01-11 21:53:37 +0800

    add: 增加dubbo demo

---
+++ ecs-dubbo/readme.md
@@ -0,0 +1,3 @@
+##简单版dubbo
+用来记录自己实现dubbo的过程
+2020-11-21 初始化dubbo模块，思考如何设计provider和consumer
--- ecs-util/src/test/java/com/gavin/app/util/TableSizeForTest.java
+++ ecs-util/src/test/java/com/gavin/app/util/TableSizeForTest.java
@@ -9,12 +9,25 @@ import java.util.HashMap;
  * @date 2020/1/17 11:00 下午
  */
 public class TableSizeForTest {
+
+    /**
+     * 判断一个数是2的整数次幂
+     * @param val
+     * @return
+     */
+    private static boolean isPowerOfTwo(int val) {
+        System.out.println("Integer.toBinaryString(val) = " + Integer.toBinaryString(val));
+        System.out.println("Integer.toBinaryString(-val) = " + Integer.toBinaryString(-val));
+        return (val & -val) == val;
+    }
+
     public static void main(String[] args) {
-        Integer i = 10;
-        System.out.println("整数前面0的个数为：" + Integer.numberOfLeadingZeros(i));
-        int n = -1 >>> Integer.numberOfLeadingZeros(i - 1);
-        System.out.println("最小的2的幂次方为：" + (i = (n < 0) ? 1  : n + 1));
-        System.out.println(-1 >>> 28);
-        System.out.println(Integer.toHexString(-1));
+//        Integer i = 10;
+//        System.out.println("整数前面0的个数为：" + Integer.numberOfLeadingZeros(i));
+//        int n = -1 >>> Integer.numberOfLeadingZeros(i - 1);
+//        System.out.println("最小的2的幂次方为：" + (i = (n < 0) ? 1  : n + 1));
+//        System.out.println(-1 >>> 28);
+//        System.out.println(Integer.toHexString(-1));
+        System.out.println("isPowerOfTwo(8) = " + isPowerOfTwo(8));
     }
 }

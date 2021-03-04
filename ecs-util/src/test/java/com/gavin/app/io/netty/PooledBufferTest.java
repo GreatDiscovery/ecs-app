37002a09927f267d66716312af314c7cbf60661b
gavin <1303535630@qq.com>
2021-03-04 12:28:09 +0800

    update: 增加多链表合并

--- ecs-util/src/test/java/com/gavin/app/io/netty/PooledBufferTest.java
+++ ecs-util/src/test/java/com/gavin/app/io/netty/PooledBufferTest.java
@@ -4,6 +4,8 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.PooledByteBufAllocator;
 import org.apache.commons.lang.StringUtils;
 
+import java.nio.charset.Charset;
+
 /**
  * @author gavin
  * @date 2020/6/21 10:00 下午
@@ -12,6 +14,9 @@ public class PooledBufferTest {
     public static void main(String[] args) {
         PooledByteBufAllocator allocator = PooledByteBufAllocator.DEFAULT;
         ByteBuf byteBuf = allocator.directBuffer(16);
+        String str = "hello";
+        byteBuf.setBytes(0, str.getBytes());
+        System.out.println(byteBuf.getCharSequence(0, 5, Charset.defaultCharset()));
         byteBuf.release();
         System.out.println(PooledBufferTest.class.getClassLoader());
     }

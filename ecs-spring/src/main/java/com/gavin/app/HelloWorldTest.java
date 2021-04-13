c6df7e306c3bb9fd96b9c3085e94e72ec467da53
gavin <1303535630@qq.com>
2021-04-13 11:47:01 +0800

    add: 增加aop

--- ecs-spring/src/main/java/com/gavin/app/HelloWorldTest.java
+++ ecs-spring/src/main/java/com/gavin/app/HelloWorldTest.java
@@ -1,11 +1,8 @@
 package com.gavin.app;
 
 import com.gavin.app.config.AppConfig;
-import com.gavin.app.service.AService;
-import com.gavin.app.service.HelloWorldService;
+import com.gavin.app.service.IHelloWorldService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.util.Arrays;
 
 /**
  * @author: Gavin
@@ -16,7 +13,7 @@ public class HelloWorldTest {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
         //通过ApplicationContext.getBean(beanName)动态加载数据（类）【获取Spring容器中已初始化的bean】。
-        HelloWorldService helloWorld=(HelloWorldService) ac.getBean("HelloWorldService");
+        IHelloWorldService helloWorld = (IHelloWorldService) ac.getBean("HelloWorldService");
 //        Arrays.stream(ac.getBeanDefinitionNames()).forEach(System.out::println);
         //执行动态加载到的类的方法
         helloWorld.sayHello();
